195dd336cfa4804c7a3913fa2f1f60ef368b68f4
Axatyr <alessandro.palladino23@gmail.com>
2022-01-24 12:42:08 +0100

    fix last thing

--- template/login-home.php
+++ template/login-home.php
@@ -1,10 +1,6 @@
 <h1><span class="fas fa-user-alt"></span> <?php echo $_SESSION["nome"]; ?></h1>
 <div class="admin">
     <div class="card">
-        <img src="upload/user/order.jpg" alt="ordini" class="image">
-        <div class="overlay"><a href="ordini.php">I miei ordini</a></div>
-    </div>
-    <div class="card">
         <img src="upload/user/avatar.jpg" alt="dati" class="image">
         <div class="overlay"><a href="dati.php">I miei dati</a></div>
     </div>
@@ -18,6 +14,11 @@
             <div class="overlay"><a href="gestione-prodotti.php">I miei prodotti</a></div>
     </div>
     <?php endif; ?>
+    <div class="card">
+        <img src="upload/user/order.jpg" alt="ordini" class="image">
+        <div class="overlay"><a href="ordini.php">I miei ordini</a></div>
+    </div>
+    
 </div>        
 <a href="logout.php"><button type="button" class="logout">Logout</button></a>
     
