f582d603e6eae2b788519cfef35b047de0707b63
Axatyr <alessandro.palladino23@gmail.com>
2022-01-26 20:48:17 +0100

    fix image user data

--- template/dati.php
+++ template/dati.php
@@ -1,7 +1,7 @@
 <h1><span class="fas fa-user-alt"></span> I miei dati</h1>
 <?php $utente= $templateParams["dati"][0]; ?>
 <div class="dati">
-    <img src="upload/user/avatar.png" class="avatar" alt="avatar">
+    <img src="upload/user/avatar.jpg" class="avatar" alt="avatar">
     <p>Nome: <?php echo $utente["Nome"]; ?></p>
     <p>Cognome: <?php echo $utente["Cognome"]; ?></p>
     <p>E-mail: <?php echo $utente["Mail"]; ?></p>
